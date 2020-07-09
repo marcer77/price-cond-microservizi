83c09ea4c6753fa739d638981962d3d5db9dfcfd
r.esposito@almaviva.it <r.esposito@almaviva.it>
2020-07-09 12:46:03 +0200

    disabilitazione test

--- core-price-cjvariazionicons-v1/src/test/java/com/intesasanpaolo/bear/cond0/cjvariazionicons/dto/InputStampaDTO_OK_Test.java
+++ core-price-cjvariazionicons-v1/src/test/java/com/intesasanpaolo/bear/cond0/cjvariazionicons/dto/InputStampaDTO_OK_Test.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.intesasanpaolo.bear.cond0.cjvariazionicons.utils.JUnitUtils;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
 public class InputStampaDTO_OK_Test extends JUnitUtils {
 
 	private Logger log = Logger.getLogger(InputStampaDTO_OK_Test.class);
