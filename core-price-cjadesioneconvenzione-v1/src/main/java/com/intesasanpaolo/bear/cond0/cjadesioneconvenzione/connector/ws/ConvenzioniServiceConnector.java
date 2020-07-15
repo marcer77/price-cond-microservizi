01af505e2590ae9dcd8142bf03dd15e1d545ddfe
mceraldi <mceraldi@almaviva.it>
2020-07-15 15:19:46 +0200

    enable tracing

--- core-price-cjadesioneconvenzione-v1/src/main/java/com/intesasanpaolo/bear/cond0/cjadesioneconvenzione/connector/ws/ConvenzioniServiceConnector.java
+++ core-price-cjadesioneconvenzione-v1/src/main/java/com/intesasanpaolo/bear/cond0/cjadesioneconvenzione/connector/ws/ConvenzioniServiceConnector.java
@@ -1,10 +1,19 @@
 package com.intesasanpaolo.bear.cond0.cjadesioneconvenzione.connector.ws;
 
+import org.springframework.oxm.Unmarshaller;
 import org.springframework.stereotype.Service;
+import org.springframework.ws.soap.SoapMessage;
 
 import com.intesasanpaolo.bear.connector.ws.connector.BaseDataPowerSoapConnector;
 
 @Service
 public class ConvenzioniServiceConnector<INPUT,OUTPUT,DTO,RESOURCE> extends BaseDataPowerSoapConnector<INPUT,OUTPUT,DTO,RESOURCE> {
 
+	@Override
+	protected void handleSoapFault(SoapMessage soapMessage, Unmarshaller unmarshaller) throws RuntimeException {
+		logger.info("handleSoapFault(  ) - START");
+		logger.info("Il servizio Soap ha restituito l'errore: "+ soapMessage.getFaultReason());
+		logger.info("handleSoapFault(  ) - END");
+		throw new RuntimeException("Il servizio Soap ha restituito l'errore: " + soapMessage.getFaultReason());
+	}
 }
