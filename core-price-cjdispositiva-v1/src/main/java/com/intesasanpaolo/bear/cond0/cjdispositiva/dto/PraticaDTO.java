78f4ce3e924b3e8eca066b6cb45ee4eac1740005
Antonio Salvati <antonio.salvati@fides.it>
2020-07-21 16:40:34 +0200

    oggetti dto

---
+++ core-price-cjdispositiva-v1/src/main/java/com/intesasanpaolo/bear/cond0/cjdispositiva/dto/PraticaDTO.java
@@ -0,0 +1,16 @@
+package com.intesasanpaolo.bear.cond0.cjdispositiva.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PraticaDTO {
+	
+	private String codSuperPratica;
+	private String codPratica;
+	private String codPropostaComm;
+	
+}
