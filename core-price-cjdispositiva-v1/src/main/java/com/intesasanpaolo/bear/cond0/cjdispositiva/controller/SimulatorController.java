d732ea9e52b2b233ce90d07eb4705e11bdd00c95
Antonio Salvati <antonio.salvati@fides.it>
2020-07-21 10:36:05 +0200

    fix controller post merge

--- core-price-cjdispositiva-v1/src/main/java/com/intesasanpaolo/bear/cond0/cjdispositiva/controller/SimulatorController.java
+++ core-price-cjdispositiva-v1/src/main/java/com/intesasanpaolo/bear/cond0/cjdispositiva/controller/SimulatorController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.intesasanpaolo.bear.cond0.cjdispositiva.controller;
 
 import java.util.HashMap;
@@ -184,41 +183,3 @@ public class SimulatorController extends CoreController {
 	}
 
 }
-=======
-package com.intesasanpaolo.bear.cond0.cjdispositiva.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.intesasanpaolo.bear.cond0.cjdispositiva.connector.ws.gen.convenzionihostservice.ResponseStoreCovenantAdesioneConvenzione;
-import com.intesasanpaolo.bear.cond0.cjdispositiva.model.ws.ReqStoreCovenantAdesioneConvenzione;
-import com.intesasanpaolo.bear.cond0.cjdispositiva.model.ws.RespStoreCovenantAdesioneConvenzione;
-import com.intesasanpaolo.bear.cond0.cjdispositiva.service.ConvenzioniHostService;
-import com.intesasanpaolo.bear.core.controller.CoreController;
-
-@RestController
-@RequestMapping("cjdispositiva")
-public class SimulatorController extends CoreController {
-	
-	@Autowired
-	private ConvenzioniHostService convenzioniHostService;
-	
-	@PostMapping(value="/call-StoreCovenantAdesioneConvenzione")
-	public ResponseEntity<RespStoreCovenantAdesioneConvenzione> callGetCovenantPerConvenzioni(@RequestBody ReqStoreCovenantAdesioneConvenzione request){
-		
-		
-		 RespStoreCovenantAdesioneConvenzione response = convenzioniHostService.storeCovenantAdesioneConvenzione(request);
-		
-		return ResponseEntity.ok(response);
-		
-		
-	}
-	
-
-}
->>>>>>> 7a844a7f445cf9531a2148a70575cb2903a10e1f
