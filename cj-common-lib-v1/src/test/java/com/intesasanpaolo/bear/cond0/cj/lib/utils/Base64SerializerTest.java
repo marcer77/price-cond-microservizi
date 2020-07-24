caa3ac70beebdd4e911dc254da2fa70be2756b2c
r.esposito@almaviva.it <r.esposito@almaviva.it>
2020-07-24 17:45:39 +0200

    variazioni - shared library

---
+++ cj-common-lib-v1/src/test/java/com/intesasanpaolo/bear/cond0/cj/lib/utils/Base64SerializerTest.java
@@ -0,0 +1,34 @@
+package com.intesasanpaolo.bear.cond0.cj.lib.utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+
+@RunWith(SpringRunner.class)
+public class Base64SerializerTest {
+
+	@Test
+	public void test() throws IOException {
+		byte[] bytes = Base64Serializer.serialize("Hello Word!!!");
+
+		JsonFactory factory = new JsonFactory();
+
+		ByteArrayOutputStream st = new ByteArrayOutputStream();
+		JsonGenerator generator = factory.createGenerator(st, JsonEncoding.UTF8);
+		Base64Serializer base64Serializer = new Base64Serializer();
+		base64Serializer.serialize("hello word", generator, null);
+
+		Assert.assertTrue(bytes != null);
+		Assert.assertTrue(st != null);
+
+	}
+
+}
