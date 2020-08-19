6a29a2659eb44158e6735f69f68a23684e9ac258
mceraldi <mceraldi@almaviva.it>
2020-08-19 13:12:00 +0200

    RollbackCovenantAdesioneConvenzione

---
+++ core-price-cjdispositiva-v1/src/main/java/com/intesasanpaolo/bear/cond0/cjdispositiva/model/ws/ReqRollbackStoreCovenantAdesioneConvenzione.java
@@ -0,0 +1,9 @@
+package com.intesasanpaolo.bear.cond0.cjdispositiva.model.ws;
+
+import lombok.Data;
+
+@Data
+public class ReqRollbackStoreCovenantAdesioneConvenzione extends ReqStoreCovenantAdesioneConvenzione {
+
+
+}
