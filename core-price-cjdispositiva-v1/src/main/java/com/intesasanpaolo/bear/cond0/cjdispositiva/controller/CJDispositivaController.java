679f67c591200560f624ef13ddd6eb503b0bd5ac
r.esposito@almaviva.it <r.esposito@almaviva.it>
2020-09-04 11:54:47 +0200

    correzione header

--- core-price-cjdispositiva-v1/src/main/java/com/intesasanpaolo/bear/cond0/cjdispositiva/controller/CJDispositivaController.java
+++ core-price-cjdispositiva-v1/src/main/java/com/intesasanpaolo/bear/cond0/cjdispositiva/controller/CJDispositivaController.java
@@ -35,25 +35,25 @@ public class CJDispositivaController extends CoreController {
 
 	@PostMapping(value = "/inserimento", produces = "application/json")
 	public ResponseEntity<EsitoResponseResource> inserimento(
-			@Valid @RequestBody(required = false) DispositivaRequestDTO dispositivaRequestDTO,
+			@Valid @RequestBody(required = true) DispositivaRequestDTO dispositivaRequestDTO,
 			@RequestHeader(value = HeaderAttribute.ISP_HEADER_COD_ABI, required = true, defaultValue = "") String codABI,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_COD_UNITA_OPERATIVA, required = false, defaultValue = "") String codUnitaOperativa,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_COD_UNITA_OPERATIVA, required = true, defaultValue = "") String codUnitaOperativa,
 			@RequestHeader(value = HeaderAttribute.ISP_HEADER_CALLER_CUSTOMER_ID, required = false, defaultValue = "") String customerID,
 			@RequestHeader(value = HeaderAttribute.ISP_HEADER_CALLER_COMPANY_ID_CODE, required = true, defaultValue = "") String callerCompanyIDCode,
 			@RequestHeader(value = HeaderAttribute.ISP_HEADER_SERVICE_COMPANY_ID_CODE, required = true, defaultValue = "") String serviceCompanyIDCode,
 			@RequestHeader(value = HeaderAttribute.ISP_HEADER_OPERATOR_INFO_USER_ID, required = true, defaultValue = "") String userID,
 			@RequestHeader(value = HeaderAttribute.ISP_HEADER_IS_VIRTUAL_USER, required = false) String isVirtualUser,
 			@RequestHeader(value = HeaderAttribute.ISP_HEADER_LANGUAGE, required = false, defaultValue = "") String language,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_SERVICE_ID, required = false, defaultValue = "") String serviceID,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_SERVICE_VERSION, required = false, defaultValue = "") String serviceVersion,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_SERVICE_ID, required = true, defaultValue = "") String serviceID,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_SERVICE_VERSION, required = true, defaultValue = "") String serviceVersion,
 			@RequestHeader(value = HeaderAttribute.ISP_HEADER_TIMESTAMP, required = true, defaultValue = "0") String timestamp,
 			@RequestHeader(value = HeaderAttribute.ISP_HEADER_TRANCACTION_ID, required = true, defaultValue = "") String transactionId,
 			@RequestHeader(value = HeaderAttribute.ISP_HEADER_APPLICATION_ID, required = true, defaultValue = "") String applicationID,
 			@RequestHeader(value = HeaderAttribute.ISP_HEADER_CALLER_PGM_NAME, required = false, defaultValue = "") String callerProgramName,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_CHANNEL_ID_CODE, required = true, defaultValue = "") String channelIDCode)
-			throws Exception {
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_CHANNEL_ID_CODE, required = true, defaultValue = "") String channelIDCode) throws Exception
+			 {
 		log.info(" - inviaPropostaV2 START: ");
-
+ 
 		ISPWebservicesHeaderType ispWebservicesHeaderType = ServiceUtil.buildISPWebservicesHeaderType()
 				.applicationID(applicationID).callerCompanyIDCode(callerCompanyIDCode)
 				.callerProgramName(callerProgramName).channelIDCode(channelIDCode).codABI(codABI)
@@ -61,36 +61,38 @@ public class CJDispositivaController extends CoreController {
 				.language(language).serviceCompanyIDCode(serviceCompanyIDCode).serviceID(serviceID).userID(userID)
 				.transactionId(transactionId).timestamp(timestamp).serviceVersion(serviceVersion).build();
 
-		CJDispositivaInserimentoCommand proposteCJPOSWSInviaPropostaV2Command = beanFactory
+		
+		CJDispositivaInserimentoCommand cjDispositivaInserimentoCommand = beanFactory
 				.getBean(CJDispositivaInserimentoCommand.class);
-		proposteCJPOSWSInviaPropostaV2Command.setDispositivaRequestDTO(dispositivaRequestDTO);
-		proposteCJPOSWSInviaPropostaV2Command.setIspWebservicesHeaderType(ispWebservicesHeaderType);
-		EsitoResponseResource esito = proposteCJPOSWSInviaPropostaV2Command.execute();
+		cjDispositivaInserimentoCommand.setDispositivaRequestDTO(dispositivaRequestDTO);
+		cjDispositivaInserimentoCommand.setIspWebservicesHeaderType(ispWebservicesHeaderType);
+		EsitoResponseResource esito = cjDispositivaInserimentoCommand.execute();
 		log.info(" - inviaPropostaV2 END: esito {" + esito.toString() + "}");
 		return ResponseEntity.ok(esito);
 	}
 
 	@PostMapping(value = "/annullo", produces = "application/json")
 	public ResponseEntity<EsitoResponseResource> annullo(
-			@Valid @RequestBody(required = false) DispositivaRequestDTO dispositivaRequestDTO,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_COD_ABI, required = true) String codABI,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_COD_UNITA_OPERATIVA, required = false) String codUnitaOperativa,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_CALLER_CUSTOMER_ID, required = false) String customerID,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_CALLER_COMPANY_ID_CODE, required = true) String callerCompanyIDCode,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_SERVICE_COMPANY_ID_CODE, required = true) String serviceCompanyIDCode,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_OPERATOR_INFO_USER_ID, required = true) String userID,
+			@Valid @RequestBody(required = true) DispositivaRequestDTO dispositivaRequestDTO,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_COD_ABI, required = true, defaultValue = "") String codABI,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_COD_UNITA_OPERATIVA, required = true, defaultValue = "") String codUnitaOperativa,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_CALLER_CUSTOMER_ID, required = false, defaultValue = "") String customerID,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_CALLER_COMPANY_ID_CODE, required = true, defaultValue = "") String callerCompanyIDCode,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_SERVICE_COMPANY_ID_CODE, required = true, defaultValue = "") String serviceCompanyIDCode,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_OPERATOR_INFO_USER_ID, required = true, defaultValue = "") String userID,
 			@RequestHeader(value = HeaderAttribute.ISP_HEADER_IS_VIRTUAL_USER, required = false) String isVirtualUser,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_LANGUAGE, required = false) String language,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_SERVICE_ID, required = false) String serviceID,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_SERVICE_VERSION, required = false) String serviceVersion,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_LANGUAGE, required = false, defaultValue = "") String language,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_SERVICE_ID, required = true, defaultValue = "") String serviceID,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_SERVICE_VERSION, required = true, defaultValue = "") String serviceVersion,
 			@RequestHeader(value = HeaderAttribute.ISP_HEADER_TIMESTAMP, required = true, defaultValue = "0") String timestamp,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_TRANCACTION_ID, required = true) String transactionId,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_APPLICATION_ID, required = true) String applicationID,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_CALLER_PGM_NAME, required = false) String callerProgramName,
-			@RequestHeader(value = HeaderAttribute.ISP_HEADER_CHANNEL_ID_CODE, required = true) String channelIDCode)
-			throws Exception {
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_TRANCACTION_ID, required = true, defaultValue = "") String transactionId,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_APPLICATION_ID, required = true, defaultValue = "") String applicationID,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_CALLER_PGM_NAME, required = false, defaultValue = "") String callerProgramName,
+			@RequestHeader(value = HeaderAttribute.ISP_HEADER_CHANNEL_ID_CODE, required = true, defaultValue = "") String channelIDCode) throws Exception
+		{
 		log.info(" - annullo START: ");
-		CJDispositivaAnnulloCommand proposteCJPOSWSRevocaPropostaCommand = beanFactory
+		
+		CJDispositivaAnnulloCommand cjDispositivaAnnulloCommand = beanFactory
 				.getBean(CJDispositivaAnnulloCommand.class);
 		
 		ISPWebservicesHeaderType ispWebservicesHeaderType = ServiceUtil.buildISPWebservicesHeaderType()
@@ -100,9 +102,9 @@ public class CJDispositivaController extends CoreController {
 				.language(language).serviceCompanyIDCode(serviceCompanyIDCode).serviceID(serviceID).userID(userID)
 				.transactionId(transactionId).timestamp(timestamp).serviceVersion(serviceVersion).build();
 		
-		proposteCJPOSWSRevocaPropostaCommand.setDispositivaRequestDTO(dispositivaRequestDTO);
-		proposteCJPOSWSRevocaPropostaCommand.setIspWebservicesHeaderType(ispWebservicesHeaderType);
-		EsitoResponseResource esito = proposteCJPOSWSRevocaPropostaCommand.execute();
+		cjDispositivaAnnulloCommand.setDispositivaRequestDTO(dispositivaRequestDTO);
+		cjDispositivaAnnulloCommand.setIspWebservicesHeaderType(ispWebservicesHeaderType);
+		EsitoResponseResource esito = cjDispositivaAnnulloCommand.execute();
 		log.info(" - annullo END: esito {" + esito.toString() + "}");
 		return ResponseEntity.ok(esito);
 	}
