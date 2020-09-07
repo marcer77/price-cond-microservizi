c509b01b9afd7fa6ed7a6921d4cc82e52d3f1e9f
r.esposito@almaviva.it <r.esposito@almaviva.it>
2020-09-07 17:09:21 +0200

    mappatura BS per OffertaConto

---
+++ core-price-cjoffertaconto-v1/src/main/java/com/intesasanpaolo/bear/cond0/cjoffertaconto/connector/ctg/config/CtgConnectorConfig.java
@@ -0,0 +1,17 @@
+package com.intesasanpaolo.bear.cond0.cjoffertaconto.connector.ctg.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import com.dsi.business.SSA_PC.integration.jdo.P_PCMYS00.C_PCMYS00;
+
+@Configuration
+public class CtgConnectorConfig {
+
+	@Bean
+	@Scope("prototype")
+	public C_PCMYS00 getPCMYS00ConnectorClient() {
+		return new C_PCMYS00();
+	}
+}
---
+++ core-price-cjoffertaconto-v1/src/main/java/com/intesasanpaolo/bear/cond0/cjoffertaconto/model/InPPRM.java
@@ -0,0 +1,19 @@
+package com.intesasanpaolo.bear.cond0.cjoffertaconto.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class InPPRM {
+	private String codPromo;
+	private String decaPromo;
+	private String decoPromo;
+	
+}
